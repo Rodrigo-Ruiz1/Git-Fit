e11861f0ca780b84a3451bb004c91972a02e10ab
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2021-08-26 17:14:39 -0400

    added new column to posts table, minor fixes

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,20 +1,21 @@
-TABLE users (
+CREATE TABLE users(
     id serial PRIMARY KEY,
     name text,
     username text,
     email text,
-    created_at timestamp
-)
+    created_at timestamp DEFAULT NOW()
+);
 
-TABLE routine (
+CREATE TABLE routine(
     id serial PRIMARY KEY,
     title text,
     user_id int REFERENCES users(id)
 );
 
-TABLE exercises (
+CREATE TABLE exercises(
     id serial PRIMARY KEY,
     exercise_id int,
+    title text,
     category text,
     sets int,
     reps int,
@@ -22,28 +23,30 @@ TABLE exercises (
     routine_id int REFERENCES routine(id)
 );
 
-TABLE posts (
+CREATE TABLE posts(
     id serial PRIMARY KEY,
     user_id int REFERENCES users(id),
     date timestamp DEFAULT NOW(),
-    public bool default false,
+    is_public bool default false,
     likes int default 0,
-    comments int default 0
+    comments int default 0,
+    content text,
+    routine_id int REFERENCES routine(id)
 );
 
-TABLE posts_likes (
+CREATE TABLE posts_likes(
     user_id int REFERENCES users(id),
     post_id int REFERENCES posts(id)
 );
 
-TABLE comments (
+CREATE TABLE comments(
     id serial PRIMARY KEY,
-    user_id int REFERENCES users(id)
+    user_id int REFERENCES users(id),
     post_id int REFERENCES posts(id),
-    body text,
+    body text
 );
 
-TABLE comments_likes (
+CREATE TABLE comments_likes(
     user_id int REFERENCES users(id),
     comment_id int REFERENCES comments(id)
 );
