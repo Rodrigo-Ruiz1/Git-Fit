7ef188e208f6968df2960cf3d9d75fdf1849a18e
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2021-08-28 17:36:27 -0400

    created basic route for profile page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './pages/Profile';
+import Feed from './components/Feed';
 import './App.css';
 
 function App() {
+  const { user } = useAuth0();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/profile">
+          <Profile currentUser={user}/>
+          <Feed />
+        </Route>
+        <LoginButton />
+        <LogoutButton />
+      </div>
+    </Router>
   );
 }
 
