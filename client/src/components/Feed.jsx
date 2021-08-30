6f7c7d244db9f9f53fcf3a63030d2a1d77164cf4
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2021-08-30 01:44:01 -0400

    renamed feed component

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Feed = () => {
+    const [feed, setFeed] = useState([]);
+
+
     const _fetchFeed = async () => {
         const response = await fetch(
             'http://127.0.0.1:3001/api/v1/posts/all/public'
             ).then(response => response.json());
             console.log('API RESPONSE: ', response);
+            setFeed([...response])
+            return response;
     }
 
     useEffect(() => {
