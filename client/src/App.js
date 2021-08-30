90554f040258b6ada62d3a0d8731e697906b5dcd
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2021-08-30 01:43:37 -0400

    quickly added routine page route

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,39 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
 import Profile from './pages/Profile';
+import Routine from './pages/Routine';
 import Feed from './components/Feed';
 import './App.css';
 
 function App() {
-  const { user } = useAuth0();
+  const { user, isAuthenticated } = useAuth0();
+
+  const fetchCurrentUser = async (username) => {
+    const response = await fetch(
+      `http://127.0.0.1:3001/api/v1/users/${username}`
+    ).then(response => response.json());
+    console.log(response)
+  }
+
+  useEffect(() => {
+    if (isAuthenticated === true) {
+      fetchCurrentUser(user.nickname)
+      console.log(user.nickname)
+    }
+  })
 
   return (
     <Router>
       <div className="App">
         <Route path="/profile">
-          <Profile currentUser={user}/>
+          <Profile currentUser={user} isAuthenticated={isAuthenticated}/>
           <Feed />
+        </Route>
+        <Route path="/routine">
+          <Routine />
         </Route>
         <LoginButton />
         <LogoutButton />
