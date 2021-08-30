68713cdcf4618328a0543afc5aa2612f0623a54e
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2021-08-30 01:44:24 -0400

    small changes to profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import { useState } from 'react'
 
-const Profile = ({currentUser}) => {
+const Profile = ({ currentUser, isAuthenticated }) => {
+    const [button, setButton] = useState('Verify Account');
 
+    const verifyUser = async (name, username, email, picture) => {
+        const response = await fetch('http://127.0.0.1:3001/api/v1/users/register', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                name: name,
+                username: username,
+                email: email,
+                picture: picture
+            })
+        });
+        const parseResponse = await response.json();
+        console.log(parseResponse);
+    }
+
+    const handleVerify = () => {
+        if (button === "Verify Account") {
+            verifyUser(currentUser.nickname, currentUser.nickname, currentUser.email, currentUser.picture);
+            setButton('Verified')
+        }
+    }
 
     return (
         <div>
             {JSON.stringify(currentUser, null, 2)}
+            {isAuthenticated === true ? (
+                <button type="button" onClick={() => handleVerify()}>{button}</button>
+
+            ) : (
+                null
+            )}
         </div>
     )
 };
